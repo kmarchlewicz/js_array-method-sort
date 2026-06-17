dd691e9cd0789c6cdd524e026638f7b20d1646e8
Krzysztof Marchlewicz <wow_chris@op.pl>
2026-06-17 19:28:08 +0200

    Solution 2

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,64 +1,54 @@
 'use strict';
-/*
- * Kod rozbudowuje prototyp tablicy o nową metodę sortującą.
- * Algorytm implmenetowany jest rekurencyjnie
- */
 
 function applyCustomSort() {
-  // Dodanie domyślnego parametru, gdy użytkownik nie poda swojego.
-  // Imitujemy domyślne zachowanie metody .sort()
-  // sortowanie stringów z tablocy UTF - 16
+  // Rozszerzamy prototyp o własną metodę sortującą
   [].__proto__.sort2 = function (
+    // Dodanie domyślnego parametru, gdy użytkownik nie poda swojego.
+    // Imitujemy domyślne zachowanie metody .sort
+    // tj. sortowanie stringów z tablicy UTF - 16
     compareFunction = (a, b) => {
       const stringA = String(a);
       const stringB = String(b);
 
-      if (stringA < stringB) {
-        return -1;
-      }
-
-      if (stringA > stringB) {
-        return 1;
-      }
-
-      return 0;
+      return stringA < stringB ? -1 : stringA > stringB ? 1 : 0;
     },
   ) {
-    const input = [...this];
-    const result = [];
+    // licznik tur. Przesuwa obszar zainteresowania
+    // poza elementy już posortowane
+    let count = 0;
 
     // Warunek zatrzymania rekurencji
     const checkLength = () => {
-      if (result.length !== this.length) {
+      if (count !== this.length) {
         subSort();
       }
     };
 
     const subSort = () => {
-      let lead = input[0];
+      let lead = this[count];
       let index = 0;
 
-      for (let i = 1; i < input.length; i++) {
-        const current = input[i];
+      for (let i = 1; i < this.length - count; i++) {
+        const current = this[i + count];
 
-        // Ustawiamy kolejność elementów a,b w nowej tablicy
+        // Ustawiamy kolejność elementów
+        // jeżeli wynik jest mniejszy od 0,
+        // to current w tablicy ma być wcześniej niż lead
         if (compareFunction(current, lead) < 0) {
           lead = current;
           index = i;
         }
       }
 
-      result.push(lead);
-      input.splice(index, 1);
+      // Manipulacja elementami tablicy in-place
+      this.splice(index + count, 1); // usuwamy znalezionego lidera
+      // dodajemy lidera w miejscu za dotychczas posortowanymi elementami
+      this.splice(count, 0, lead);
+      count++;
       checkLength();
     };
 
     subSort();
-
-    // Modyfikacja tablicy wejściowej in-place
-    for (let i = 0; i < result.length; i++) {
-      this[i] = result[i];
-    }
 
     return this;
   };
