826e4960ab963db55de1d94caa712fc5b6f64b47
Krzysztof Marchlewicz <wow_chris@op.pl>
2026-06-20 16:46:32 +0200

    Solution 3

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -1,11 +1,10 @@
 'use strict';
 
 function applyCustomSort() {
-  // Rozszerzamy prototyp o własną metodę sortującą
+  // Dodanie domyślnego parametru, gdy użytkownik nie poda swojego.
+  // Imitujemy domyślne zachowanie metody .sort
+  // sortowanie stringów z tablocy UTF - 16
   [].__proto__.sort2 = function (
-    // Dodanie domyślnego parametru, gdy użytkownik nie poda swojego.
-    // Imitujemy domyślne zachowanie metody .sort
-    // tj. sortowanie stringów z tablicy UTF - 16
     compareFunction = (a, b) => {
       const stringA = String(a);
       const stringB = String(b);
@@ -13,42 +12,17 @@ function applyCustomSort() {
       return stringA < stringB ? -1 : stringA > stringB ? 1 : 0;
     },
   ) {
-    // licznik tur. Przesuwa obszar zainteresowania
-    // poza elementy już posortowane
-    let count = 0;
+    for (let i = 0; i < this.length; i++) {
+      let lead = i;
 
-    // Warunek zatrzymania rekurencji
-    const checkLength = () => {
-      if (count !== this.length) {
-        subSort();
-      }
-    };
-
-    const subSort = () => {
-      let lead = this[count];
-      let index = 0;
-
-      for (let i = 1; i < this.length - count; i++) {
-        const current = this[i + count];
-
-        // Ustawiamy kolejność elementów
-        // jeżeli wynik jest mniejszy od 0,
-        // to current w tablicy ma być wcześniej niż lead
-        if (compareFunction(current, lead) < 0) {
-          lead = current;
-          index = i;
+      for (let j = i + 1; j < this.length; j++) {
+        if (compareFunction(this[j], this[lead]) < 0) {
+          lead = j;
         }
       }
-
-      // Manipulacja elementami tablicy in-place
-      this.splice(index + count, 1); // usuwamy znalezionego lidera
-      // dodajemy lidera w miejscu za dotychczas posortowanymi elementami
-      this.splice(count, 0, lead);
-      count++;
-      checkLength();
-    };
-
-    subSort();
+      // zamiana miejsc dwóch elementów w tabeli
+      [this[i], this[lead]] = [this[lead], this[i]];
+    }
 
     return this;
   };
